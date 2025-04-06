869c1ee117075f49b94ab032639302689bdbc8a9
cmrocktest <cmrock85cl@gmail.com>
2025-04-06 01:03:38 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@
 const CACHE_NAME = 'rtf-app-v3';
 
 const FILES_TO_CACHE = [
-  'index.html',
-  'manifest.json',
-  'icon.png',
-  'instructivo-uso-miniscape.mp4'
+  'index.html?v=3',
+  'manifest.json?v=3',
+  'icon.png?v=3',
+  'instructivo-uso-miniscape.mp4?v=3'
 ];
 
 self.addEventListener('install', function(e) {
-  self.skipWaiting(); // activa al instante
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(FILES_TO_CACHE);
@@ -22,15 +22,24 @@ self.addEventListener('activate', function(e) {
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== CACHE_NAME) {
-          return caches.delete(key); // borra caché anterior
+          return caches.delete(key);
         }
       }));
     })
   );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
+  const url = new URL(e.request.url);
+  const cleanRequest = new Request(url.pathname, {
+    method: e.request.method,
+    headers: e.request.headers,
+    mode: e.request.mode,
+    credentials: e.request.credentials,
+    redirect: e.request.redirect
+  });
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(cleanRequest))
   );
 });
